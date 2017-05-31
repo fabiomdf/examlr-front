af562acb40a6751f1461ad096490c92836bed50c
fabiomdf <fabiomdf@gmail.com>
2017-05-31 00:03:53 -0300

    0.0.2

--- src/app/simulation/simulation.component.ts
+++ src/app/simulation/simulation.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Group } from "../group/group";
 import { Question } from "../question/question";
 import { QuestionService } from "../question/question.service";
+import { Answer } from "../answer/answer";
 
 @Component({
     moduleId: module.id,
@@ -10,10 +11,10 @@ import { QuestionService } from "../question/question.service";
     templateUrl: 'simulation.component.html'
 })
 export class SimulationComponent implements OnInit {
-    
-    index: number;
-    questions: any[];
-    questionsSimulate: any[];
+
+    qindex: number;
+    questions: Array<any>;
+    questionsSimulate: Array<any>;
     group: Group;
 
     @Input()
@@ -29,14 +30,40 @@ export class SimulationComponent implements OnInit {
     getQuestionsByGroupId(): void {
         this.questionService
             .getQuestionsByGroupId(this.group.id)
-            .then(result => { 
+            .then(result => {
                 this.questions = result;
+                this.setAnswersClear(result);
             });
     }
 
+    setAnswersClear(questions: Array<any>): void {
+        // Cloning the object
+        this.questionsSimulate = JSON.parse(JSON.stringify(questions));
+        // Set all answers to false
+        this.questionsSimulate.forEach(q => {
+            q.answers.forEach(a => {
+                a.correctAnswer = false;
+            });
+        });
+    }
+
+    setAnswerCheck(qindex: number, aindex: number, itemChecked: boolean): void {
+        this.questionsSimulate[qindex].answers[aindex].correctAnswer = itemChecked;
+    }
+
+    finishSimulation(): void {
+        let i = 0;
+        this.questions.forEach(element => {
+            if (JSON.stringify(element) == JSON.stringify(this.questionsSimulate[i])) {
+                console.log(`Pregunta: ${i} está Correta`);
+            } else {
+                console.log(`Pregunta: ${i} está Errada`);
+            }
+            i++;
+        });
+    }
+
     ngOnInit() {
-        this.index = 0;
-     }
-
-
+        this.qindex = 0;
+    }
 }
